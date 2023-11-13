83269ccfbe422b258a42a627a578a7f8efdde429
jungAh Han <truth.h406@gmail.com>
2023-11-13 11:11:48 +0900

    add: 이미지 인덱스에 맞춰 버튼 보이기

--- carousel.js
+++ carousel.js
@@ -2,11 +2,20 @@ const carousel = document.querySelector(".carousel");
 const btnPrev = document.querySelector(".carousel-btn.prev");
 const btnNext = document.querySelector(".carousel-btn.next");
 
+let currentIndex = 0;
+const lastIndex = 2;
+
+document.addEventListener("DOMContentLoaded", () => {
+  visibilitySlideButtons(currentIndex);
+});
+
 btnPrev.addEventListener("click", () => {
   carousel.scrollBy({
     left: -carousel.clientWidth,
     behavior: "smooth",
   });
+  currentIndex--;
+  visibilitySlideButtons(currentIndex);
 });
 
 btnNext.addEventListener("click", () => {
@@ -14,4 +23,19 @@ btnNext.addEventListener("click", () => {
     left: carousel.clientWidth,
     behavior: "smooth",
   });
+  currentIndex++;
+  visibilitySlideButtons(currentIndex);
 });
+
+function visibilitySlideButtons(currentIndex) {
+  if (currentIndex === 0) {
+    btnPrev.style.display = "none";
+    btnNext.style.display = "block";
+  } else if (currentIndex === 1) {
+    btnPrev.style.display = "block";
+    btnNext.style.display = "block";
+  } else {
+    btnPrev.style.display = "block";
+    btnNext.style.display = "none";
+  }
+}
