20d4a6634ac46072c4f16e08f4a8459ae00aff29
jungAh Han <truth.h406@gmail.com>
2023-11-17 10:18:03 +0900

    문제화 위한 코드 합치기

--- header.js
+++ header.js
@@ -1,7 +1,3 @@
 document.getElementById("logo").addEventListener("click", () => {
   window.location.href = "./index.html";
 });
-
-document.getElementById("button-cart").addEventListener("click", () => {
-  window.location.href = "./cart.html";
-});
