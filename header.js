f89458980f0b16f0e7265137975e098c3b2177f6
truthone <truth.h406@gmail.com>
2024-01-19 14:59:54 +0900

    delete files

--- header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('logo').addEventListener('click', () => {
-    window.location.href = './index.html';
-  });
-
-  document.getElementById('cart-button').addEventListener('click', () => {
-    window.location.href = './cart.html';
-  });
-});
